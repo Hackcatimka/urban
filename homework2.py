a7f5a8a37dac37da8dbac2399fdbed00ce03ca7e
Hackcatimka <128597925+Hackcatimka@users.noreply.github.com>
2024-07-17 13:05:58 +0300

    Variables

---
+++ homework2.py
@@ -0,0 +1,10 @@
+
+number_task = 12
+
+number_out_Hour = 1.5
+
+Course_name = "Python"
+
+time_ts = number_out_Hour/number_task
+
+print(f"Курс {Course_name}, всего задача: {number_task}, затрачено часов: {number_out_Hour}, среднее время выполнения {time_ts} часа.")
