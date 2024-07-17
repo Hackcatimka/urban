212eecc9cf6244fc2987421624515965c469dfa6
Hackcatimka <128597925+Hackcatimka@users.noreply.github.com>
2024-07-17 13:19:28 +0300

    Program organization and string methods

---
+++ homework4.py
@@ -0,0 +1,14 @@
+
+my_string = input("Введите значение: ")
+
+print("Длина строки: ", len(my_string))
+
+print("В верхнем регистре: ", my_string.upper())
+
+print("В нижнем регистре: ", my_string.lower())
+
+print("Без пробелов: ", my_string.replace(' ', ''))
+
+print("Первый символ строки: ", my_string[0])
+
+print("Последний символ строки: ", my_string[-1])
