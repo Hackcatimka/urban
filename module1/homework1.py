c4e7b4f76514b3f28723903d7237e57baf4793a4
Hackcatimka <128597925+Hackcatimka@users.noreply.github.com>
2024-07-18 11:30:11 +0300

    Update homework1.py

--- module1/homework1.py
+++ module1/homework1.py
@@ -1,8 +1,7 @@
-example = "Круто"
+example = "Уникальный"
 
-print("Слово Примернный \nПервый символ: ", example[0])
+print("Слово Уникальный \nПервый символ: ", example[0])
 print("Последний символ: ", example[-1])
-mid = int(len(example)/2)
-print("Вторая половина строки: ", example[mid::])
+print("Вторая половина строки: ", example[5::])
 print("Слово наоборот: ", example[::-1])
-print("Каждый второй символ: ", example[::2])
+print("Каждый второй символ: ", example[1::2])
