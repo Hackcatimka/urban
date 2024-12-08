7acd37006993c4148a25b2cb21c89758614f686f
Hackcatimka <128597925+Hackcatimka@users.noreply.github.com>
2024-12-08 14:50:11 +0300

    Add files via upload

---
+++ module 7/module_7.py
@@ -0,0 +1,50 @@
+# Входные данные
+team1_num = 5
+team2_num = 6
+score_1 = 40
+score_2 = 42
+team1_time = 1552.512
+team2_time = 2153.31451
+tasks_total = 82
+time_avg = 45.2
+
+# Определяем результат соревнования
+if score_1 > score_2 or (score_1 == score_2 and team1_time > team2_time):
+    challenge_result = "Победа команды Мастера кода!"
+elif score_1 < score_2 or (score_1 == score_2 and team1_time < team2_time):
+    challenge_result = "Победа команды Волшебники данных!"
+else:
+    challenge_result = "Ничья!"
+
+# Использование % форматирования
+# Количество участников первой команды
+team1_string = "В команде Мастера кода участников: %d !" % team1_num
+
+# Количество участников в обеих командах
+teams_total_string = "Итого сегодня в командах участников: %d и %d !" % (team1_num, team2_num)
+
+# Использование format()
+# Количество задач, решённых командой 2
+team2_score_string = "Команда Волшебники данных решила задач: {} !".format(score_2)
+
+# Время, за которое команда 2 решила задачи
+team2_time_string = "Волшебники данных решили задачи за {:.2f} с !".format(team2_time)
+
+# Использование f-строк
+# Количество решённых задач по командам
+tasks_solved_string = f"Команды решили {score_1} и {score_2} задач."
+
+# Исход соревнования
+challenge_result_string = f"Результат битвы: {challenge_result}"
+
+# Общее количество задач и среднее время решения
+summary_string = f"Сегодня было решено {tasks_total} задач, в среднем по {time_avg:.1f} секунды на задачу!."
+
+# Вывод результатов
+print(team1_string)
+print(teams_total_string)
+print(team2_score_string)
+print(team2_time_string)
+print(tasks_solved_string)
+print(challenge_result_string)
+print(summary_string)
